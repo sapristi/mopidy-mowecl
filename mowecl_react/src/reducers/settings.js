20c95cebb6dcd4eb92158cede0e21562e4cbdf3d
sapristi <megabigfoutoir@gmail.com>
2020-03-11 07:02:35 +0100

    settings validation

--- mowecl_react/src/reducers/settings.js
+++ mowecl_react/src/reducers/settings.js
@@ -7,17 +7,20 @@ const defaultPersistantSettings = {
     mopidy_ws: {
         name: 'Modidy WebSocker URL',
         default: getDefaultWS(),
-        help: 'Modidy WebSocker URL. Do not modify unless you know what you are doing.'
+        help: 'Modidy WebSocker URL. Do not modify unless you know what you are doing.',
+        validate: v => v
     },
     seek_update_interval: {
         name: "Seek update interval",
         default: 500,
         help: 'Time interval (ms) at which the song progress bar will update.',
+        validate: v => parseInt(v) || 500
     },
     search_history_length: {
         name: "Search history length",
         default: 10,
         help: 'Number of items in search history. Set 0 to disable.',
+        validate: v => parseInt(v) || 10
     }
 }
 
@@ -59,10 +62,17 @@ export const settingsReducer = (state=defaultSettings, action) => {
                              JSON.stringify(
                                  Object.fromEntries(
                                      Object.entries(action.data).map(
-                                         ([k,v]) => [k, v.current]
+                                         ([k,v]) => [k, v.validate(v.current)]
                                      ))))
         // console.log("Set", state.persistant)
-        return {...state, persistant: action.data}
+        return {...state, persistant:
+
+                Object.fromEntries(
+                    Object.entries(action.data).map(
+                        ([k, v]) => [k, {...v, current: v.validate(v.current)}]
+                    )
+                )
+                }
 
     default:
         return state
