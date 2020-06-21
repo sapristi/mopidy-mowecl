e688595a308338915e45d4c0d912713035cf1251
sapristi <megabigfoutoir@gmail.com>
2020-06-21 18:27:05 +0200

    remove bookmarks

--- mopidy_mowecl/__init__.py
+++ mopidy_mowecl/__init__.py
@@ -14,28 +14,6 @@ __version__ = pkg_resources.get_distribution("Mopidy-Mowecl").version
 
 logger = logging.getLogger(__name__)
 
-
-class ApiHandler(RequestHandler):
-    keys = ["settings"]
-
-    def check_request(self, arg, request):
-        return arg in self.keys and len(request.body) < 10000
-
-    def initialize(self, data_dir, allowed_origins):
-        self.data_dir = data_dir
-        self.allowed_origins = allowed_origins
-
-    def get(self, arg):
-        self.set_header("Access-Control-Allow-Origin", "*")
-        res = store.load(self.data_dir, arg)
-        self.write(res)
-
-    def post(self, arg):
-        self.set_header("Access-Control-Allow-Origin", "*")
-        req = self.request
-        if check_request(arg, req):
-            store.save(self.data_dir, "settings", req.body)
-
 class Extension(ext.Extension):
 
     dist_name = "Mopidy-Mowecl"
@@ -58,29 +36,3 @@ class Extension(ext.Extension):
                 "path": str(pathlib.Path(__file__).parent / "static"),
             },
         )
-        registry.add(
-            "http:app", {
-                "name": self.ext_name,
-                "factory": self.http_app_factory}
-        )
-        registry.add("frontend", bookmarks.MopidyCoreListener)
-
-
-    def http_app_factory(self, config, core):
-        allowed_origins = {
-            x.lower() for x in config["http"]["allowed_origins"] if x
-        }
-
-        store.initialize(self.get_data_dir(config))
-        return [
-            (
-                r"/api/(.*)",
-                ApiHandler,
-                {"data_dir": self.get_data_dir(config),
-                 "allowed_origins": allowed_origins}
-            ),
-            (
-                r"/ws/?", bookmarks.WebSocketHandler, {}
-            )
-        ]
-
