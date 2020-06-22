309a3639b1110088e8ad82ee88fb6579d79159cf
sapristi <megabigfoutoir@gmail.com>
2020-06-23 00:29:09 +0200

    final fixes

--- mopidy_mowecl/file_server.py
+++ mopidy_mowecl/file_server.py
@@ -3,15 +3,20 @@ import logging
 
 logger = logging.getLogger(__name__)
 
+def sanitize_config(config):
+    def sanitize_param(p):
+        return p if p else ""
+    return {k: sanitize_param(v) for k, v in config.items()}
+
 class FileServer(tornado.web.RequestHandler):
     def initialize(self, config, path):
-
         self.config = config
         self.path = path
 
     def get(self, path=None):
+        config_sanitized = sanitize_config(self.config["mowecl"])
         template_params = {
-            **self.config["mowecl"],
+            **config_sanitized,
             "static_settings_enabled": "true"
         }
         return self.render("index.html", title="Mowecl", **template_params)
