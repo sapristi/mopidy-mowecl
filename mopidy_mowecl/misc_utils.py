52b8884a37c6a295590b172875bf03b864e1d502
Mathias Millet <mathias@mmill.eu>
2025-03-25 22:30:12 +0000

    feat: add lastfm url in artist data

--- mopidy_mowecl/misc_utils.py
+++ mopidy_mowecl/misc_utils.py
@@ -15,12 +15,13 @@ class LastFMWrapper:
     def get_artist_data(self, artist_name):
         resp = self.client.search_for_artist(artist_name)
         page = resp.get_next_page()
-        first_result = page[0]
-        bio = first_result.get_bio_content()
-        name = first_result.get_name()
-        listener_count = first_result.get_listener_count()
+        artist = page[0] # only considering first result for now
+        bio = artist.get_bio_content()
+        name = artist.get_name()
+        listener_count = artist.get_listener_count()
         return {
             "bio": bio,
             "name": name,
-            "listener_count": listener_count
+            "listener_count": listener_count,
+            "url": artist.get_url()
         }
