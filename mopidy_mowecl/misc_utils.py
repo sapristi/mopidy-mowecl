034fe4ab547220510f63ee17d03bac8bacb28a1b
Mathias Millet <mathias@mmill.eu>
2025-03-28 19:33:18 +0000

    feat: better lastfm search

--- mopidy_mowecl/misc_utils.py
+++ mopidy_mowecl/misc_utils.py
@@ -15,7 +15,14 @@ class LastFMWrapper:
     def get_artist_data(self, artist_name):
         resp = self.client.search_for_artist(artist_name)
         page = resp.get_next_page()
-        artist = page[0] # only considering first result for now
+        artist = None
+        for result in page:
+            if result.get_name().lower() == artist_name.lower():
+                artist = result
+                break
+        else:
+            artist = page[0]
+
         bio = artist.get_bio_content()
         name = artist.get_name()
         listener_count = artist.get_listener_count()
